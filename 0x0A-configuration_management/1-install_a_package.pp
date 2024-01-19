7647502e4e3d1a8acde7d875e0b4ba5face13712
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2024-01-19 10:44:40 -0800

    auto pushing

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Install flask with 2.1.0 version
+# Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
