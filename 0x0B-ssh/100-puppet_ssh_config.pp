5ec179be41cf48151d6b0ed6c57c1fb294c0ebc5
Mohammed Mustafa <mohammed112025@gmail.com>
2024-01-20 13:58:11 -0800

    auto pushing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # configure ssh configuration using puppet
 file_line {'Turn off pas auth':
 	path => '/etc/ssh/ssh_config',
-	line => 'PasswordAuthenticatation no'
+	line => 'PasswordAuthentication no'
 }
 
 file_line {'Turn on auth with pk':
